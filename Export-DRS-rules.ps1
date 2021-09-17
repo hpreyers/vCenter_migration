afca122f67ef207fe0733641c7f77c4294ade7c5
Harold Preyers <harold.preyers@vconsultants.be>
2021-09-17 10:57:50 +0200

    Added menu

--- Export-DRS-rules.ps1
+++ Export-DRS-rules.ps1
@@ -1,5 +1,15 @@
 ﻿$viserver = 'tanzu-vcsa-1.tanzu.demo'
-$clusterName = 'tanzu-cluster'
+$clusters = Get-Cluster
+Do {
+  $i = 1
+  foreach ($cluster in $clusters){
+    Write-Host "$i. $($cluster.Name)"
+    $i = $i + 1
+  }
+  [int]$clusterNumber = Read-Host -Prompt "Enter the number for the cluster"
+} Until ($clusterNumber -in 1..$clusters.Count) # -and ($clusterNumber -lt 1))
+write $clusters.count
+$clusterName = ($clusters[$clusterNumber]).Name
 
 $outfileVMToHosts = "c:\temp\drs-rules-vm_to_hosts.txt"
 Remove-Item $outfileVMToHosts
